dcfd57cb4c118f46afde027bfe1e5f361a15170d
Joao Duarte <jsvduarte@gmail.com>
2016-01-05 17:31:08 +0000

    print settings at startup

--- logstash-core/lib/logstash/pipeline.rb
+++ logstash-core/lib/logstash/pipeline.rb
@@ -37,10 +37,9 @@ module LogStash; class Pipeline
     :config_str,
     :original_settings
 
-  DEFAULT_OUTPUT_WORKERS = 1
-
   DEFAULT_SETTINGS = {
     :default_pipeline_workers => LogStash::Config::CpuCoreStrategy.maximum,
+    :default_output_workers => 1,
     :pipeline_batch_size => 125,
     :pipeline_batch_delay => 5, # in milliseconds
     :flush_interval => 5, # in seconds
@@ -48,6 +47,7 @@ module LogStash; class Pipeline
     :debug_config => false,
     :allow_env => false
   }
+
   MAX_INFLIGHT_WARN_THRESHOLD = 10_000
 
   RELOAD_INCOMPATIBLE_PLUGINS = [
@@ -113,6 +113,7 @@ module LogStash; class Pipeline
     # in-flight buffers
     @input_queue_pop_mutex = Mutex.new
     @input_threads = []
+    @settings = LogStash::DEFAULT_SETTINGS.clone
     # @ready requires thread safety since it is typically polled from outside the pipeline thread
     @ready = Concurrent::AtomicBoolean.new(false)
     @running = Concurrent::AtomicBoolean.new(false)
@@ -128,7 +129,7 @@ module LogStash; class Pipeline
   end
 
   def safe_pipeline_worker_count
-    default = DEFAULT_SETTINGS[:default_pipeline_workers]
+    default = LogStash::DEFAULT_SETTINGS[:default_pipeline_workers]
     thread_count = @settings[:pipeline_workers] #override from args "-w 8" or config
     safe_filters, unsafe_filters = @filters.partition(&:threadsafe?)
 
@@ -460,7 +461,7 @@ module LogStash; class Pipeline
     klass = LogStash::Plugin.lookup(plugin_type, name)
 
     if plugin_type == "output"
-      LogStash::OutputDelegator.new(@logger, klass, DEFAULT_OUTPUT_WORKERS, pipeline_scoped_metric.namespace(:outputs), *args)
+      LogStash::OutputDelegator.new(@logger, klass, DEFAULT_SETTINGS[:default_output_workers], pipeline_scoped_metric.namespace(:outputs), *args)
     elsif plugin_type == "filter"
       LogStash::FilterDelegator.new(@logger, klass, pipeline_scoped_metric.namespace(:filters), *args)
     else
